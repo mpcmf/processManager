163c101399ae38741bc36945257decbc41b02faf
isaitkulov <saitkulovim@gmail.com>
2018-12-10 18:16:34 +0300

    [importProcesses] use apiClient

--- apps/processHandler/commands/processHandler/importProcesses.php
+++ apps/processHandler/commands/processHandler/importProcesses.php
@@ -2,11 +2,9 @@
 
 namespace mpcmf\apps\processHandler\commands\processHandler;
 
-use mpcmf\modules\processHandler\mappers\processMapper;
-use mpcmf\modules\processHandler\mappers\serverMapper;
-use mpcmf\modules\processHandler\models\processModel;
-use mpcmf\modules\processHandler\models\serverModel;
+use mpcmf\apps\processHandler\libraries\api\client\apiClient;
 use mpcmf\system\application\consoleCommandBase;
+use mpcmf\system\cache\cache;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -17,6 +15,8 @@ use Symfony\Component\Console\Output\OutputInterface;
 class importProcesses
     extends consoleCommandBase
 {
+    //3 weeks
+    protected $cacheExpire = 1814400;
 
     /**
      * Define arguments
@@ -33,73 +33,33 @@ class importProcesses
         $jsonPath = $input->getOption('json_path');
 
         if (!$jsonPath) {
-            $jsonPath = __DIR__ . '/../processesList.json';
+            $jsonPath = __DIR__ . '/../../libraries/processesList.json';
         }
         $processes = json_decode(file_get_contents($jsonPath), true);
+
         if (empty($processes)) {
             exit('Empty processes list!');
         }
-
         $this->saveProcesses($processes);
     }
 
     protected function saveProcesses($processes)
     {
-        if (!is_array($processes)) {
-            exit('Processes list must me array!');
-        }
-
-        $processMapper = processMapper::getInstance();
-        $saved = 0;
+        $apiClient = apiClient::factory();
         foreach ($processes as $process) {
-            $process['server'] = 'tmpServer';
-            $result = processModel::validate($process);
-            unset($process['server']);
+            $cacheKey = 'importProcesses_' . md5(preg_replace('/\s/', '', $process['command']) . 'import');
+            if (cache::getCached($cacheKey)) {
+                echo "[WARNING][{$process['command']}] already imported!\n";
+                continue;
+            }
+
+            $result = $apiClient->call('process', 'add', ['object' => $process]);
             if (!$result['status']) {
-                error_log('Errors on validation fields:');
-                foreach ($result['errors'] as $errors) {
-                    foreach ($errors as $error) {
-                        error_log($error);
-                    }
-                }
-
-                var_dump($process);
-                exit;
+                var_dump($result);
+                continue;
             }
-            $process['server'] = $this->getServerIdByHost($process['host']);
-            $processModel = processModel::fromArray($process);
-            try {
-                $processMapper->save($processModel);
-                $saved++;
-            } catch (\Exception $exception) {
-                error_log("[Exception] on saving process: {$exception->getMessage()}");
-            }
+            echo "[SUCCESS][{$process['command']}]\n";
+            cache::setCached($cacheKey, true, 1814400);
         }
-
-        error_log("Created {$saved} processes!");
-    }
-
-    protected function getServerIdByHost($host)
-    {
-        $host = trim($host);
-        $serverMapper = serverMapper::getInstance();
-
-        try {
-            /** @var serverModel $model */
-            $model = $serverMapper->getBy([
-                serverMapper::FIELD__HOST => $host
-            ]);
-        } catch (\Exception $exception) {
-            if  (mb_strpos($exception->getMessage(), 'Item not found') !== false) {
-                $model = serverModel::fromArray([
-                    serverMapper::FIELD__HOST => $host,
-                    serverMapper::FIELD__NAME => '',
-                ]);
-                $serverMapper->save($model);
-            }
-
-        }
-
-        return (string) $model->getMongoId();
     }
 }
