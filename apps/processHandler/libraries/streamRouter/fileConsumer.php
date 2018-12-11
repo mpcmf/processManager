640ee7e3f0b5e5f50eb36171293ad4eb21f84290
isaitkulov <saitkulovim@gmail.com>
2018-12-11 11:09:42 +0300

    [fileConsumer] check file is writable

--- apps/processHandler/libraries/streamRouter/fileConsumer.php
+++ apps/processHandler/libraries/streamRouter/fileConsumer.php
@@ -7,6 +7,20 @@ class fileConsumer
 {
     public function consume($data)
     {
+        static $notWritable = [];
+
+        if (isset($notWritable[$this->destination])) {
+            if (!is_writable($this->destination)) {
+                error_log("File {$this->destination} not writable!");
+                return;
+            }
+            unset($notWritable[$this->destination]);
+        }
+
         file_put_contents($this->destination, $data, FILE_APPEND);
+
+        if (!is_writable($this->destination)) {
+            $notWritable[$this->destination] = $this->destination;
+        }
     }
 }
